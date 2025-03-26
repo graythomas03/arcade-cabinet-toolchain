2a3f2ec036d32309c7c66738ebfe9e5eb46c5676
Gray Thomas <graythomas03@protonmail.com>
2025-03-25 20:26:22 -0400

    .desktop files generation

--- autoloader/autoloader.py
+++ autoloader/autoloader.py
@@ -7,62 +7,62 @@ Description:
     the application
 """
 
-import os
-import sys
+import os # give me Linux
+import fileinput # give me File
+import tomllib # give me Config format
+
+HOME_DIR = os.getenv('HOME') # should be /home/vgdc
+ICON_DIR = os.getenv('ARCADE_ICON_DIR') # should be /home/vgdc/menu/icons
+APP_DIR = os.getenv('ARCADE_APP_DIR') # should be /home/vgdc/apps/
+MENU_DIR = os.getenv('ARCADE_MENU_DIR') # should be /home/vgdc/menu/entries
+
+DESKTOP_ENTRY_FILE_FMT = """
+[Desktop Entry]
+Name={0}
+Type=Application
+Terminal=false
+Exec={1}
+"""
+
+# FORMAT: display_name;icon_name;app_name
+# MENU_ENTRY_FMT = 'Entry{0}={1};' + ICON_DIR + '{2}.png;' + MENU_DIR + '{3}'
 
 class Manifest:
-    _type
+    type : str
     name : str
     name_display : str
     icon : str
+    executable : str
     menu_path : str
     
-    def __init__(self):
-        pass
+    def __init__(self, type, name, name_display, icon, executable, menu_path):
+        self.type = type
+        self.name = name
+        self.name_display = name_display
+        self.icon = icon
+        self.executable = executable
+        self.menu_path = menu_path
 
-# read manifest files found at app installtion directory
-def cache_manifests( root_app_dir : str ) -> list :
-    # enter every directory found in the root_dir
-
-    # if dir contains .manifest file, read info
+def create_menu_entry_string():
     pass
 
-def write_menu_changes( manifest_list ):
-    pass
+def create_desktop_entry( app: Manifest ):
+    # skip Manifest entry if Link or Category
+    if app.type == 'App':
+        # abs_exec_path = HOME_DIR + '/' + APP_DIR + '/' + app.name + '/' + app.executable
+        abs_exec_path = app.executable
+        # open new desktop file with same name as manifest
+        # new_desktop_entry_path = "%s/apps/%s.desktop".format(HOME_DIR, app.name)
+        new_desktop_entry_path = app.name + '.desktop'
+        with open(new_desktop_entry_path, 'w+') as fp:
+            fp.write( DESKTOP_ENTRY_FILE_FMT.format( app.name, abs_exec_path ) )
+    else:
+        print( 'Skipping entry build for non-app manifest', app.name )
 
 def main():
-
-    # Process any cmd args
-
-    # Search for all local app entries
-
-    ### APP PATHS
-    root_app_dir = ""
-    root_icon_dir = ""
-    manifest_file_ext = "*.manifest"
-    root_bkup_dir = ""
-
-    incoming_app_dir = ""
-
-    ### CACHED ENTRIES
-    entries = cache_manifests( root_app_dir )
-
-    # Install/load new games from incoming USB storage device
-
-    new_entries = cache_manifests( incoming_app_dir )
-    for new_entry in new_entries:
-        print("Loading manifest for app '{}'...", new_entry.name)
-        
-        # check if App has been previously loaded
-        for entry in entries:
-            if new_entry.name == entry.name:
-                print("[WARNING] Existing manifest for app '{}' found. Creating backup...", new_entry.name)
-                # backup and compress files
-
-        # move files to destination
-
-    # update menu and config files
-
-    write_menu_changes()
+    test_manifest_0 = Manifest('App', 'Test0', 'DisplayTest', 'Debug', 'TestExec', '')
+    test_manifest_1 = Manifest('Link', 'Test1', 'DisplayTest', 'Debug', 'TestExec', '')
+    create_desktop_entry( test_manifest_0 )
+    create_desktop_entry( test_manifest_1 )
 
 main()
