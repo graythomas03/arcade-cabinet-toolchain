bd18e97c3b94442ab13546210c1977d007f25641
Gray Thomas <graythomas03@protonmail.com>
2025-03-26 02:27:51 -0400

    Added manifest parsing

--- autoloader/autoloader.py
+++ autoloader/autoloader.py
@@ -15,7 +15,9 @@ HOME_DIR = os.getenv('HOME') # should be /home/vgdc
 ICON_DIR = os.getenv('ARCADE_ICON_DIR') # should be /home/vgdc/menu/icons
 APP_DIR = os.getenv('ARCADE_APP_DIR') # should be /home/vgdc/apps/
 MENU_DIR = os.getenv('ARCADE_MENU_DIR') # should be /home/vgdc/menu/entries
+WEB_BROWSER = ''
 
+# According to the Free Desktop standard
 DESKTOP_ENTRY_FILE_FMT = """
 [Desktop Entry]
 Name={0}
@@ -24,8 +26,12 @@ Terminal=false
 Exec={1}
 """
 
+MIN_SUPPORTED_VERSION=1.0
+
 # FORMAT: display_name;icon_name;app_name
 # MENU_ENTRY_FMT = 'Entry{0}={1};' + ICON_DIR + '{2}.png;' + MENU_DIR + '{3}'
+# MENU_ENTRY_FMT = '{1};' + ICON_DIR + '/{2}.png;' + MENU_DIR + '/{3}'
+MENU_ENTRY_FMT = '{0};{1}.png;{2}' # debugging case
 
 class Manifest:
     type : str
@@ -35,7 +41,7 @@ class Manifest:
     executable : str
     menu_path : str
     
-    def __init__(self, type, name, name_display, icon, executable, menu_path):
+    def __init__(self, type, name : str, name_display : str, icon : str, executable : str, menu_path : str):
         self.type = type
         self.name = name
         self.name_display = name_display
@@ -43,10 +49,18 @@ class Manifest:
         self.executable = executable
         self.menu_path = menu_path
 
-def create_menu_entry_string():
-    pass
+def get_entry_str( m : Manifest ) -> str:
+    cmd = None
+    if m.type == 'App' or m.type == 'Link':
+        cmd = m.name.join('.desktop')
+    elif m.type == 'Category':
+        cmd = ':submenu ' + m.name
+    else:
+        print( 'Unknown type', m.type, 'for manifest', m.name )
+        return ''
+    return MENU_ENTRY_FMT.format(m.name_display, m.icon, cmd)
 
-def create_desktop_entry( app: Manifest ):
+def create_desktop_entry( app: Manifest ) -> None:
     # skip Manifest entry if Link or Category
     if app.type == 'App':
         # abs_exec_path = HOME_DIR + '/' + APP_DIR + '/' + app.name + '/' + app.executable
@@ -58,6 +72,43 @@ def create_desktop_entry( app: Manifest ):
             fp.write( DESKTOP_ENTRY_FILE_FMT.format( app.name, abs_exec_path ) )
     else:
         print( 'Skipping entry build for non-app manifest', app.name )
+
+def read_manifests_from_dir( path : str ) -> list[Manifest]:
+    for entry in os.scandir(path):
+        if entry.is_file():
+            try:
+                with open(entry) as fp:
+                    manifest_data = tomllib.load(fp)
+
+                    try:
+                        if float(manifest_data['VGDC_ARCADE']) >= MIN_SUPPORTED_VERSION:
+
+                            if manifest_data['App'] != None:
+                                manifest_obj = Manifest( 'App', 
+                                                        '',
+                                                        manifest_data['DisplayName'],
+                                                        manifest_data['Icon'],
+                                                         manifest_data['Exec'],
+                                                          manifest_data['MenuPath'] )
+                                
+                            elif manifest_data['Link'] != None:
+                                manifest_obj = Manifest( 'App', 
+                                                        manifest_data['Name'],
+                                                        manifest_data['DisplayName'],
+                                                        manifest_data['Icon'],
+                                                         manifest_data['Exec'],
+                                                          manifest_data['MenuPath'] )
+                                
+                            elif manifest_data['Category'] != None:
+                                pass
+                            else:
+                                print( 'Unknown type in manifest file',  entry)
+
+                    except:
+                        print( 'Invalid version value for manifest file', entry )
+                        continue
+            except:
+                continue
 
 def main():
     test_manifest_0 = Manifest('App', 'Test0', 'DisplayTest', 'Debug', 'TestExec', '')
